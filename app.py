c7daada9158015dee7430caf5709e9082e42f000
MubaraOnGit <immohammedmubark@gmail.com>
2025-04-06 17:21:50 +0400

    String

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-# variables
-students_count = 1000
-rating = 4.99
-is_published = False
-course_name = 'Python Programing'
+# strings
+course = 'Python Programming'
+print(len(course))
+print(course[0])
+print(course[-1]) # end of the string
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
 
-# make variables names meaningfully
+course = 'Python \"Programming' # escape character
+course = 'Python \nProgramming' # new line
+print(course)
 
-# lower case and use '_' to separate
+first = 'Mohammed'
+last = 'Mubarak'
+full = first + ' ' + last
+print(full)
+full = f"{first} {last}"
+print(full)
+
+course = 'Python Programming'
+print(course.upper()) # with '.' after a function we can find available methods
+print(course) # the original not effected
+print(course.lower())
+print(course.title())
+print(course.strip()) # trim the space end and starting
+print(course.find('pro'))
+print(course.replace('P', 'j'))
+print('Pro' in course) # True
+print('Pro' not in course) # False
+
