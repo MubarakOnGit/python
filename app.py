bf24108d09721181168c37cbd053c8a1afd62ec2
MubaraOnGit <immohammedmubark@gmail.com>
2025-04-06 16:59:08 +0400

    variables

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-print('Hello world')
+# variables
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = 'Python Programing'
 
+# make variables names meaningfully
 
-
-
-
+# lower case and use '_' to separate
